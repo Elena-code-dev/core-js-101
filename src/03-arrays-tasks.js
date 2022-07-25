84b67475b5fcc17ca0c12e17003ae8dc9b67d79e
Elena-code-dev <kev.76@inbox.ru>
2022-07-25 22:34:56 +0300

    feat: add 03-arrays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -621,8 +621,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const length = Math.floor(arr.length / 2);
+  const res = arr.splice(0, length);
+  const res2 = arr.splice(arr.length - length, length);
+  return res2.concat(arr, res);
 }
 
 
