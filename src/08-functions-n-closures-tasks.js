5a5b652f1dbfbbb75a267cb0461fe496c69e9420
Elena-code-dev <kev.76@inbox.ru>
2022-07-26 20:34:18 +0300

    feat: add 08

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,10 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function (num) {
+    return num ** exponent;
+  };
 }
 
 
@@ -62,10 +64,9 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(X) {
+  return (a) => (b) => (c) => a * X ** 2 + b * X + c;
 }
-
 
 /**
  * Memoizes passed function and returns function
